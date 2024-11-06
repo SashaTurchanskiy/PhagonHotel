f8a376e7f869b72625e81ecd5177d61d147bca8b
Sasha Turchanskiy <turchanskiy777@gmail.com>
2024-11-06 10:12:35 +0200

    Added project on Git

---
+++ src/test/java/com/Phagondev/PhagonHotel/PhagonHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Phagondev.PhagonHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhagonHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
