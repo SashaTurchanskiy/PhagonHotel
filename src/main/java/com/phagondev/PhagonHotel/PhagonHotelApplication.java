d6921455d132da6f150ce2800f0ce59faf7e4b9b
Sasha Turchanskiy <turchanskiy777@gmail.com>
2025-05-19 10:10:44 +0300

    set up project

---
+++ src/main/java/com/phagondev/PhagonHotel/PhagonHotelApplication.java
@@ -0,0 +1,13 @@
+package com.phagondev.PhagonHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhagonHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhagonHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phagondev/PhagonHotel/PhagonHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phagondev.PhagonHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhagonHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
