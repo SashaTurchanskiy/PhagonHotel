3796c6e548598e103299b827e14c6f260edb318f
Sasha Turchanskiy <turchanskiy777@gmail.com>
2025-05-28 12:16:54 +0300

    finished implementing room service

--- src/main/java/com/phagondev/PhagonHotel/service/impl/RoomService.java
+++ src/main/java/com/phagondev/PhagonHotel/service/impl/RoomService.java
@@ -3,6 +3,7 @@ package com.phagondev.PhagonHotel.service.impl;
 import com.phagondev.PhagonHotel.dto.Response;
 import com.phagondev.PhagonHotel.dto.RoomDto;
 import com.phagondev.PhagonHotel.entity.Room;
+import com.phagondev.PhagonHotel.exception.OurException;
 import com.phagondev.PhagonHotel.repo.BookingRepo;
 import com.phagondev.PhagonHotel.repo.RoomRepo;
 import com.phagondev.PhagonHotel.service.AwsS3Service;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -71,26 +73,112 @@ public class RoomService implements IRoomService {
 
     @Override
     public Response deleteRoom(Long roomId) {
-        return null;
+        Response response = new Response();
+
+        try {
+            roomRepo.findById(roomId).orElseThrow(() -> new OurException("Room not found"));
+            response.setStatusCode(200);
+            response.setMessage("Rooms fetched successfully");
+        }catch (OurException e){
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+        }catch (Exception e){
+            response.setStatusCode(500);
+            response.setMessage("Error getting all rooms " + e.getMessage());
+        }
+        return response;
+
     }
 
     @Override
-    public Response updateRoom(Long roomId, MultipartFile photo, String roomType, BigDecimal roomPrice) {
-        return null;
+    public Response updateRoom(Long roomId, String roomDescription, MultipartFile photo, String roomType, BigDecimal roomPrice) {
+        Response response = new Response();
+
+        try {
+            String imageUrl = null;
+            if (photo != null && !photo.isEmpty()) {
+                imageUrl = awsS3Service.saveImageToS3(photo);
+            }
+            Room room = roomRepo.findById(roomId).orElseThrow(()-> new OurException("Room not found"));
+            if (roomType != null) room.setRoomType(roomType);
+            if (roomPrice != null) room.setRoomPrice(roomPrice);
+            if (roomDescription != null) room.setRoomDescription(roomDescription);
+            if (imageUrl != null) room.setRoomPhotoUrl(imageUrl);
+
+            Room updatedRoom = roomRepo.save(room);
+            RoomDto roomDto = Utils.mapRoomEntityToRoomDTO(updatedRoom);
+
+            response.setStatusCode(200);
+            response.setMessage("Room updated successfully");
+            response.setRoom(roomDto);
+
+        }catch (OurException e){
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+        }catch (Exception e){
+            response.setStatusCode(500);
+            response.setMessage("Error getting all rooms " + e.getMessage());
+        }
+        return response;
     }
 
     @Override
     public Response getRoomById(Long roomId) {
-        return null;
+        Response response = new Response();
+
+        try {
+            Room room = roomRepo.findById(roomId).orElseThrow(()-> new OurException("Room not found"));
+            RoomDto roomDto = Utils.mapRoomEntityToRoomDTOPlusBookings(room);
+
+            response.setStatusCode(200);
+            response.setMessage("Rooms fetched successfully");
+            response.setRoom(roomDto);
+
+        }catch (OurException e){
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+        }catch (Exception e){
+            response.setStatusCode(500);
+            response.setMessage("Error getting all rooms " + e.getMessage());
+        }
+        return response;
     }
 
     @Override
-    public Response getAvailableRoomsByDataAndType(String checkInDate, String checkOutDate, String roomType) {
-        return null;
+    public Response getAvailableRoomsByDataAndType(LocalDate checkInDate, LocalDate checkOutDate, String roomType) {
+        Response response = new Response();
+
+        try {
+            List<Room> availableRooms = roomRepo.findAvailableRoomsByDatesAndTypes(checkInDate, checkOutDate, roomType);
+            List<RoomDto> roomDto = Utils.mapRoomListEntityToRoomListDTO(availableRooms);
+
+            response.setStatusCode(200);
+            response.setMessage("Rooms fetched successfully");
+            response.setRoomList(roomDto);
+
+        }catch (Exception e){
+            response.setStatusCode(500);
+            response.setMessage("Error getting all rooms " + e.getMessage());
+        }
+        return response;
     }
 
     @Override
     public Response getAllAvailableRooms() {
-        return null;
+        Response response = new Response();
+
+        try {
+            List<Room> roomList = roomRepo.getAllAvailableRooms();
+            List<RoomDto> roomDto = Utils.mapRoomListEntityToRoomListDTO(roomList);
+
+            response.setStatusCode(200);
+            response.setMessage("Rooms fetched successfully");
+            response.setRoomList(roomDto);
+
+        }catch (Exception e){
+            response.setStatusCode(500);
+            response.setMessage("Error getting all rooms " + e.getMessage());
+        }
+        return response;
     }
 }
--- src/main/java/com/phagondev/PhagonHotel/service/interfac/IRoomService.java
+++ src/main/java/com/phagondev/PhagonHotel/service/interfac/IRoomService.java
@@ -4,6 +4,7 @@ import com.phagondev.PhagonHotel.dto.Response;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface IRoomService {
@@ -11,8 +12,8 @@ public interface IRoomService {
     List<String> getAllRoomTypes();
     Response getAllRooms();
     Response deleteRoom(Long roomId);
-    Response updateRoom(Long roomId, MultipartFile photo, String roomType, BigDecimal roomPrice);
+    Response updateRoom(Long roomId, String roomDescription, MultipartFile photo, String roomType, BigDecimal roomPrice);
     Response getRoomById(Long roomId);
-    Response getAvailableRoomsByDataAndType(String checkInDate, String checkOutDate, String roomType);
+    Response getAvailableRoomsByDataAndType(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
     Response getAllAvailableRooms();
 }
